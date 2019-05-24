cf62ae6082b12ba50dbbb474e12f861572fb5987
sergio-zuniga <sergio.zuniga@usach.cl>
2019-05-24 22:08:51 +0000

    se agregan los archivos de seed, factory y migration

---
+++ database/factories/tipo_pagoFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/* @var $factory \Illuminate\Database\Eloquent\Factory */
+
+use App\Tipo_pago;
+use Faker\Generator as Faker;
+
+$factory->define(Tipo_pago::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
---
+++ database/factories/valoracionFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/* @var $factory \Illuminate\Database\Eloquent\Factory */
+
+use App\Valoracion;
+use Faker\Generator as Faker;
+
+$factory->define(Valoracion::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});

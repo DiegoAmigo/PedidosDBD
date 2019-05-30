f50b3219d8eccdaee71b232749037be41eb063aa
Unknown <cristobal.cisterna@usach.cl>
2019-05-30 16:54:42 -0400

    Se modifican factory

--- database/factories/valoracionFactory.php
+++ database/factories/valoracionFactory.php
@@ -7,6 +7,8 @@ use Faker\Generator as Faker;
 
 $factory->define(Valoracion::class, function (Faker $faker) {
     return [
+    	'estrella' => $faker->randomFloat($nbMaxDecimals = 3, $min = 0, $max = 5),
+    	'comentario'=>$faker->text($maxNbChars= 200)
         //
     ];
 });

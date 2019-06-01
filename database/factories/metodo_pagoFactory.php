01c9635b86a3a844de799004e24fae64a9c1cc03
Unknown <cristobal.cisterna@usach.cl>
2019-06-01 16:45:40 -0400

    Se modifica metodo_pago factory

--- database/factories/metodo_pagoFactory.php
+++ database/factories/metodo_pagoFactory.php
@@ -2,15 +2,30 @@
 
 /* @var $factory \Illuminate\Database\Eloquent\Factory */
 
-use App\Model;
+use App\Metodo_pago;
 use Faker\Generator as Faker;
 
-$factory->define(Model::class, function (Faker $faker) {
+$factory->define(Metodo_pago::class, function (Faker $faker) {
+	$pago_entrega =  $faker->boolean($chanceOfGettingTrue = 50);
+	if ( $pago_entrega == true)
+	{
+		$pago_tarjeta = false;
+		$numero_tarjeta = NULL;
+		$fecha_vencimiento = NULL;
+		$digitos_verificadores = NULL;
+	}
+	else
+	{
+		$pago_tarjeta = true;
+		$numero_tarjeta = $faker->creditCardNumber;
+		$fecha_vencimiento = $faker->creditCardExpirationDate;
+		$digitos_verificadores = $faker->creditCardExpirationDate;
+	}
     return [
     	'pago_entrega'=> $faker->boolean($chanceOfGettingTrue = 50),
-    	'pago_tarjeta'=> $faker->boolean($chanceOfGettingTrue = 50),
-    	'numero_tarjeta'=>$faker->creditCardNumber,
-    	'fecha_vencimiento'=>$faker->creditCardExpirationDate,
+    	'pago_tarjeta'=> $pago_tarjeta,
+    	'numero_tarjeta'=>$numero_tarjeta,
+    	'fecha_vencimiento'=>$fecha_vencimiento,
     	'digitos_verificadores' => $faker->numberBetween($min = 0, $max = 9999)
 
         //
--- database/factories/pedido_metodo_pagoFactory.php
+++ database/factories/pedido_metodo_pagoFactory.php
@@ -2,10 +2,10 @@
 
 /* @var $factory \Illuminate\Database\Eloquent\Factory */
 
-use App\Model;
+use App\Pedido_Metodo_pago;
 use Faker\Generator as Faker;
 
-$factory->define(Model::class, function (Faker $faker) {
+$factory->define(Pedido_Metodo_pago::class, function (Faker $faker) {
 	$id_pedido = DB::table('pedidos')->select('id')->get();
     $id_metodo_pago = DB::table('metodo_pagos')->select('id')->get();
     return [

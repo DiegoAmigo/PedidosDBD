2617b663e9ecdc2e895ab4456cdc2a4bf4a73642
Unknown <cristobal.cisterna@usach.cl>
2019-06-06 15:26:58 -0400

    Se agregan seed faltantes

---
+++ database/seeds/menu_pedidoTableSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class menu_pedidoTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}

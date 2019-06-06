d3682a25a3f4f050faa42327ae4059e30efea9a1
Unknown <diego.amigo@usach.cl>
2019-06-06 17:30:18 -0400

    fix factory rol - permiso

--- database/factories/permisoFactory.php
+++ database/factories/permisoFactory.php
@@ -12,10 +12,14 @@ $factory->define(Permiso::class, function (Faker $faker) {
     $gestionar_plataforma = $faker->boolean($chanceOfGettingTrue = 10);
     $gestionar_menus = $faker->boolean($chanceOfGettingTrue = 20);
     if($gestionar_local == true || $gestionar_menus == true){
+        $gestionar_local = true;
+        $gestionar_menus = true;
         $gestionar_locales = false;
         $gestionar_plataforma = false;
     }
     if($gestionar_locales == true || $gestionar_plataforma == true){
+        $gestionar_locales = true;
+        $gestionar_plataforma = true;
         $gestionar_local = false;
         $gestionar_menus = false;
     }
@@ -24,6 +28,6 @@ $factory->define(Permiso::class, function (Faker $faker) {
         'gestionar_locales'=>$gestionar_locales,
         'gestionar_plataforma'=>$gestionar_plataforma,
         'gestionar_menus'=>$gestionar_menus,
-
+        'id_rol'=>$id_rol->random()->id
     ];
 });
--- database/factories/rolFactory.php
+++ database/factories/rolFactory.php
@@ -6,8 +6,9 @@ use App\Rol;
 use Faker\Generator as Faker;
 
 $factory->define(Rol::class, function (Faker $faker) {
+
     return [
         'tipo_rol'=>$faker->randomElement(['Administrador','Administrador de local','Usuario']),
-        
+
     ];
 });

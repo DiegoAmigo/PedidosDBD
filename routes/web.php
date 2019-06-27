6f3c9cfbd0f4e9e93561d138a9eb7ea752fb8cf7
Unknown <diego.amigo@usach.cl>
2019-06-27 02:05:30 -0400

    Inicio ya de laravel con vue, auth creado y linkeado con vue component

--- routes/web.php
+++ routes/web.php
@@ -73,3 +73,7 @@ Route::resource('/valoraciones', 'ValoracionController');
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
